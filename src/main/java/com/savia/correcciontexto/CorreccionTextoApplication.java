5b4312c42d3e000842e617535e22b1aa8fbc290d
Juan Morales <juanmr2311@gmail.com>
2024-08-08 19:08:23 -0500

    Initial commit

---
+++ src/main/java/com/savia/correcciontexto/CorreccionTextoApplication.java
@@ -0,0 +1,13 @@
+package com.savia.correcciontexto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorreccionTextoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorreccionTextoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savia/correcciontexto/CorreccionTextoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savia.correcciontexto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreccionTextoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
